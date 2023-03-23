6af1e7e7c8648e200016c253deaae607ca13236d
ArjenCC <113308394+ArjenCC@users.noreply.github.com>
2023-03-23 12:44:11 -0400

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+//connecting to the mongodb survey_answer database
+module.exports = 
+{
+    "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/survey_answer?retryWrites=true&w=majority"
+}
